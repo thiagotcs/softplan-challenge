e6dce62690512d3463e07d05dbf7c8041e4d1591
Thiago Carvalho <thiagotcs.dev@gmail.com>
2020-10-03 18:06:41 -0300

    feat: cria lista de card com dados da api

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 
 import { Card } from 'antd';
 
+import './styles.css';
+
 const { Meta } = Card;
 
 const CountryCard = ({ countryName, capital, countryFlag }) => (
-  <Card hoverable cover={<img alt="country flag" src={countryFlag} />}>
+  <Card
+    hoverable
+    className="card"
+    cover={<img alt="country flag" className="flag" src={countryFlag} />}
+  >
     <Meta title={countryName} description={capital} />
   </Card>
 );
--- src/pages/Countries/index.js
+++ src/pages/Countries/index.js
@@ -1,5 +1,42 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { Row, Col } from 'antd';
+
+import './styles.css';
 
 import { Card } from '../../components';
 
-export default () => <Card countryName="Brasil" capital="Brasilia" />;
+const GET_COUNTRIES = gql`
+  query {
+    Country {
+      name
+      capital
+      flag {
+        svgFile
+      }
+    }
+  }
+`;
+
+export default () => {
+  const { data, loading, error } = useQuery(GET_COUNTRIES);
+
+  if (loading) return <h1>loading...</h1>;
+  if (error) return <h1>error..</h1>;
+
+  return (
+    <div className="container">
+      <Row gutter={[24, 16]}>
+        {data?.Country.map((country) => (
+          <Col xs={{ span: 12 }} sm={{ span: 12 }} lg={{ span: 6 }} xl={{ span: 6 }}>
+            <Card
+              countryName={country.name}
+              capital={country.capital}
+              countryFlag={country.flag.svgFile}
+            />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
